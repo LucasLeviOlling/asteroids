116cf2a0aa6cde57cba8c76fc1df6ae3fc000265
Lucas <lucas.olling@gmail.com>
2025-12-16 16:33:02 +0100

    Added basic player drawing functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
 import constants
+import player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -10,13 +11,23 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    avatar = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        avatar.draw(screen)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+
+
+
 
 if __name__ == "__main__":
     main()
